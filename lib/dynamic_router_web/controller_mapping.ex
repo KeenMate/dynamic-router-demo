c6344258591c66aa52a3db9e4f0f7d0ff9a8bea2
Albert Moravec <albert.moravec@gmail.com>
2022-03-28 23:15:41 +0200

    Add Static controller

--- lib/dynamic_router_web/controller_mapping.ex
+++ lib/dynamic_router_web/controller_mapping.ex
@@ -2,7 +2,8 @@ defmodule DynamicRouterWeb.ControllerMapping do
   @map %{
     "news" => DynamicRouterWeb.NewsController,
     "article" => DynamicRouterWeb.ArticleController,
-    "download" => DynamicRouterWeb.DownloadController
+    "download" => DynamicRouterWeb.DownloadController,
+    "static" => DynamicRouterWeb.StaticController
   }
 
   def controller_mapping() do
